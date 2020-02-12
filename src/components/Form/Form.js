c55ade591a334f00c935ce808189d0981cb00fa8
Melanie Freshness <david.alex.thompson@gmail.com>
2020-02-12 18:06:16 +0000

    Send email works

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,54 +1,30 @@
 import React from 'react';
+import emailjs from 'emailjs-com';
 
-export default class extends React.Component {
-	constructor(props) {
-		super(props);
-		this.state = { feedback: '', name: 'David', email: 'david.alex.thompson@gmail.com' };
-		this.handleChange = this.handleChange.bind(this);
-		this.handleSubmit = this.handleSubmit.bind(this);
-	}
+export default function Form() {
+	function sendEmail(e) {
+		e.preventDefault();
 
-	render() {
-		return (
-			<form className="test-mailing">
-				<h1>Let's see if it works</h1>
-				<div>
-					<textarea
-						id="test-mailing"
-						name="test-mailing"
-						onChange={this.handleChange}
-						placeholder="Post some lorem ipsum here"
-						required
-						value={this.state.feedback}
-						style={{ width: '100%', height: '150px' }}
-					/>
-				</div>
-				<input type="button" value="Submit" className="btn btn--submit" onClick={this.handleSubmit} />
-			</form>
+		emailjs.sendForm('gmail', 'template_tDVh4JGR', e.target, 'user_qSZvdg7MBAMXbZIPsDdmJ').then(
+			(result) => {
+				console.log(result.text);
+			},
+			(error) => {
+				console.log(error.text);
+			}
 		);
 	}
 
-	handleChange(event) {
-		this.setState({ feedback: event.target.value });
-	}
-
-	handleSubmit(event) {
-		const templateId = 'template_id';
-
-		this.sendFeedback(templateId, {
-			message_html: this.state.feedback,
-			from_name: this.state.name,
-			reply_to: this.state.email
-		});
-	}
-
-	sendFeedback(templateId, variables) {
-		window.emailjs
-			.send('gmail', templateId, variables)
-			.then((res) => {
-				console.log('Email successfully sent!');
-			})
-			// Handle errors here however you like, or use a React error boundary
-			.catch((err) => console.error('Oh well, you failed. Here some thoughts on the error that occured:', err));
-	}
+	return (
+		<form className="contact-form" onSubmit={sendEmail}>
+			<input type="hidden" name="contact_number" />
+			<label>Name</label>
+			<input type="text" name="user_name" />
+			<label>Email</label>
+			<input type="email" name="user_email" />
+			<label>Message</label>
+			<textarea name="message" />
+			<input type="submit" value="Send" />
+		</form>
+	);
 }
